14e68f61f78fdda3806c76ae44d00d14a716c616
Sean Gilligan <sean@msgilligan.com>
2016-07-23 19:20:03 -0700

    Desktop: Set non-resizable window size and title

--- desktop/src/org/iosdevcamp/mermadia/desktop/DesktopLauncher.java
+++ desktop/src/org/iosdevcamp/mermadia/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import org.iosdevcamp.mermadia.MermaniaGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Mermadia";
+		// Set width/height to 2/5 iPhone 6s Plus size
+        config.resizable = false;
+		config.width = 1080*2/5;
+		config.height = 1920*2/5;
+
 		new LwjglApplication(new MermaniaGame(), config);
 	}
 }
