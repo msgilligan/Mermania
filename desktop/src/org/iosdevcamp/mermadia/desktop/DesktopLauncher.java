9ae24c8b600dcca68790b96e8d8079fd54a80ecf
Sean Gilligan <sean@msgilligan.com>
2016-07-23 17:00:10 -0700

    Created with LibGDX setup program

---
+++ desktop/src/org/iosdevcamp/mermadia/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.iosdevcamp.mermadia.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.iosdevcamp.mermadia.MermaniaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MermaniaGame(), config);
+	}
+}
